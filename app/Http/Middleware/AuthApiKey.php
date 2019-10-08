dcb7aa76564efe37828a5f63c4495d9817695f03
Mehedi Hasan <mehedi1055@gmail.com>
2019-10-08 19:25:44 +0600

    init

--- app/Http/Middleware/AuthApiKey.php
+++ app/Http/Middleware/AuthApiKey.php
@@ -15,7 +15,7 @@ class AuthApiKey
         $headers=$request->headers->all();
         if(!empty($headers['api-key'][0])) {
             $has_access = $this->findApiKey($headers['api-key'][0]);
-            if(!$has_access) return response('Unauthorized Api Key or Time Limit Bound', 401);
+            if(!$has_access['no_error']) return response($has_access['message'], 401);
         }
         else{
             return response('Unauthorized Api Key or Include api Key', 401);
@@ -26,6 +26,10 @@ class AuthApiKey
 
 
     private function findApiKey($api_key){
+        $error =[
+            'no_error'=>true,
+            'message'=>"No Error",
+        ];
         $debug = env('APP_DEBUG');
         if($debug) {
             $api_key = TokenApiKey::where('api_key', 'like', $api_key)
@@ -37,33 +41,44 @@ class AuthApiKey
                 ->where('debug', 'like', 'false')
                 ->first();
             if($api_key) {
-                if ($api_key->pricing_plan_id == 1) {
-                    if ($api_key->next_call > time()) return false;
+                if ($api_key->pricing_plan_id == 1 and $api_key->next_call != "") {
+                    if ($api_key->next_call > time()){
+                        $error['no_error'] = false;
+                        $error['message'] = "According to FREE plan ".env('API_CALL_GAP')." seconds gap between 2 consecutive requests. Try again after";
+                    }
+                    $api_key->next_call = time() + env('API_CALL_GAP');
+                }else{
                     $api_key->next_call = time() + env('API_CALL_GAP');
                 }
             }
         }
         if($api_key) {
-            $res = $this->updateApiCall($api_key->id, $api_key->pricing_plan_id);
+            $error = $this->updateApiCall($api_key->id, $api_key->pricing_plan_id);
             $api_key->increment('total_call');
             $api_key->save();
-            return $res;
+            return $error;
         }
-        return false;
+        return $error;
     }
     private function updateApiCall($api_key_id, $pricing_id){
+        $error =[
+            'no_error'=>true,
+            'message'=>"No Error",
+        ];
         $this_month = date('Y-m');
         $api_call = ApiCallCount::firstOrCreate(['token_api_key_id'=>$api_key_id], ['month_year'=>$this_month]);
         $api_call->increment('total_call');
         //for debug true don't check pricing plan
         if(env('APP_DEBUG')) {
             $api_call->save();
-            return true;
+            return $error;
         }
         if($api_call) {
             if ($pricing_id == 1) {
                 if ($api_call->total_call > env('PRICING_MONTHLY_LIMIT_1')) {
-                    return false;
+                    $error['no_error'] = false;
+                    $error['message'] = "Developer FREE Limit Exceeds";
+                    return $error;
                 }
             } elseif ($pricing_id == 2) {
                 // Will bear extra requests cost
@@ -80,6 +95,6 @@ class AuthApiKey
         }
 
         $api_call->save();
-        return true;
+        return $error;
     }
 }
