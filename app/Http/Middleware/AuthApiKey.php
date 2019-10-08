1a067279d07cb952fc2bd6ff47168716403765d0
Mehedi Hasan <mehedi1055@gmail.com>
2019-10-08 19:42:20 +0600

    init

--- app/Http/Middleware/AuthApiKey.php
+++ app/Http/Middleware/AuthApiKey.php
@@ -42,12 +42,12 @@ class AuthApiKey
                 ->first();
             if($api_key) {
                 if ($api_key->pricing_plan_id == 1 and $api_key->next_call != "") {
-                    //if ($api_key->next_call > time()){
+                    if ($api_key->next_call > time()){
                         $error['no_error'] = false;
                         $error['message'] = "According to FREE plan ".env('API_CALL_GAP')." seconds gap between 2 consecutive requests. Try again after";
                         $error['message'] = time()." ".(time() + env('API_CALL_GAP'));
                         return $error;
-                    //}
+                    }
                     $api_key->next_call = time() + env('API_CALL_GAP');
                 }else{
                     $api_key->next_call = time() + env('API_CALL_GAP');
